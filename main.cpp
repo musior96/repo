cae2680651076c3aec1bbe598208f3d0c9f96478
musior96 <musior96@interia.pl>
2020-02-29 18:31:59 +0100

    add cmakelists.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
 int main() {
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
+    cout<<"hi"<<endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
